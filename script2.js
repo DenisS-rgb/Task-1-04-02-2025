4cee2c7fbab6978487a232d7e66b1fd8425565ca
Denis.S <denissoltan700@gmail.com>
2025-02-07 15:34:24 +0200

    jQuery
    Schimbarea totala a codului din JS Vanilla in jQuery

--- script2.js
+++ script2.js
@@ -1,21 +1,16 @@
-const usersGrid = document.getElementById("usersGrid");
-const spinLoader = document.getElementById("spinLoader");
+$(document).ready(function() {
+    $("#usersGrid").css("opacity", "0");
 
-async function fetchUsers() {
-    spinLoader.classList.remove("hidden");
+    setTimeout(() => {
+        $("#spinLoader").fadeOut(100);
 
-    const response = await fetch("https://gorest.co.in/public/v2/users");
-    const users = await response.json();
-
-    spinLoader.classList.add("hidden");
-    usersGrid.style.opacity = "1";
-
-    users.forEach(user => {
-        const userCard = document.createElement("div");
-        userCard.className = `user-card ${user.status}`;
-        userCard.textContent = user.name;
-        usersGrid.appendChild(userCard);
-    });
-}
-
-fetchUsers();
+        $.getJSON("https://gorest.co.in/public/v2/users", function(data) {
+            let usersGrid = $("#usersGrid");
+            data.forEach(user => {
+                let userCard = $("<div>").addClass("user-card " + user.status).text(user.name);
+                usersGrid.append(userCard);
+            });
+            usersGrid.fadeTo(500, 1);
+        });
+    }, 1000);
+});
